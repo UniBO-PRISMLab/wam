cbbd4015fef04d9f5c2230d99bb70280b8079624
reluc <relu.cri@gmail.com>
2019-11-07 22:38:59 +0100

    feat: first draft of build command

---
+++ src/commands/build.ts
@@ -0,0 +1,16 @@
+import { rollup } from 'rollup';
+import { nodeResolve } from 'rollup-plugin-node-resolve';
+
+export async function build() {
+  const bundle = await rollup({
+    input: '../testWade/src/thing.js',
+    plugins: [nodeResolve()],
+  });
+
+  // console.log(bundle.watchFiles); // an array of file names this bundle depends on
+
+  await bundle.write({
+    file: '../testWade/dist/thing.bundle.js',
+    format: 'iife',
+  });
+}
--- src/wade.ts
+++ src/wade.ts
@@ -2,6 +2,7 @@
 import * as program from 'commander';
 
 import { init } from './commands/init';
+import { build } from './commands/build';
 
 program.name('Wade').version('0.1.0');
 
@@ -10,6 +11,12 @@ program
   .description('initialize a new project in a directory (default: ".")')
   .action(init)
   .alias('i');
+
+program
+  .command('build')
+  .description('package the application in a single Thing Application Script')
+  .action(build)
+  .alias('b');
 
 program.parse(process.argv);
 
