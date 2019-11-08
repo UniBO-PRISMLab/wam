74839c11d6cd749180cdf31283d9beb67e099a0b
reluc <relu.cri@gmail.com>
2019-11-08 15:51:54 +0100

    fix: bundle output file and code style

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -4,15 +4,12 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 export async function build() {
   const bundle = await rollup({
     input: '../testWade/src/thing.js',
-    plugins: [
-      nodeResolve(),
-    ],
+    plugins: [nodeResolve()],
   });
-
-  // console.log(bundle.watchFiles); // an array of file names this bundle depends on
 
   await bundle.write({
     file: '../testWade/dist/thing.bundle.js',
-    format: 'iife',
+    format: 'cjs',
+    sourcemap: true,
   });
 }
